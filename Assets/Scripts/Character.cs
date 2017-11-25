790f95de873437217e2820c2be8a62ceecac9600
JyWung <translivie@gmail.com>
2017-11-26 01:05:01 +0900

    update

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,17 +8,17 @@ public abstract class Character : MonoBehaviour
     protected Rigidbody2D rigidBody;
     protected Animator animator;
     //character property
-    protected float attack;
-	protected float critAttack;
-	protected float critAttackRate;
-    protected float attackCD;
-    protected float attakRange;
-    protected float damage;
-    protected float defense;
-    protected float maxHealth;
-    protected float currentHealth;
-    protected float maxSpeed;
-    protected float speed;
+    public float attack;
+	public float critAttack;
+	public float critAttackRate;
+    public float attackCD;
+    public float attakRange;
+    public float damage;
+    public float defense;
+    public float maxHealth;
+    public float currentHealth;
+    public float maxSpeed;
+    public float speed;
 
     protected bool isFacingRight;
     //character action
@@ -30,8 +30,9 @@ public abstract class Character : MonoBehaviour
 	void Awake()
 	{
         rigidBody = GetComponent<Rigidbody2D>();
+        isFacingRight = transform.localScale.x > 0 ? true : false;
     }
-    protected virtual void dead()
+    public virtual void dead()
 	{
         Destroy(this);
     }
