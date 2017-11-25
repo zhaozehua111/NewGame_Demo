a2c4dbb3f42a80ce7c81ba6070ddf1914c3d6013
JyWung <translivie@gmail.com>
2017-11-26 00:48:07 +0900

    base class

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+    //unity component
+    protected Rigidbody2D rigidBody;
+    protected Animator animator;
+    //character property
+    protected float attack;
+	protected float critAttack;
+	protected float critAttackRate;
+    protected float attackCD;
+    protected float attakRange;
+    protected float damage;
+    protected float defense;
+    protected float maxHealth;
+    protected float currentHealth;
+    protected float maxSpeed;
+    protected float speed;
+
+    protected bool isFacingRight;
+    //character action
+    protected abstract void move();
+    protected abstract void jump();
+    protected abstract void addAttack();
+    protected abstract void addDamage();
+	
+	void Awake()
+	{
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    protected virtual void dead()
+	{
+        Destroy(this);
+    }
+
+	protected void overturn()
+	{
+		isFacingRight = !isFacingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+}
---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : Character {
+	void Start()
+	{
+	}
+    protected override void move()
+	{
+		float move = Input.GetAxis("Horizontal");
+		rigidBody.velocity = new Vector2(move * maxSpeed, rigidBody.velocity.y);
+
+		//速度方向為正且未朝右的情況下，翻轉sprite
+		if (move > 0 && !isFacingRight)
+		{
+			base.overturn();
+		}
+		//反之
+		else if (move < 0 && isFacingRight)
+		{
+			base.overturn();
+		}
+	}
+    protected override void jump()
+	{
+
+	}
+    protected override void addAttack()
+	{
+
+	}
+    protected override void addDamage()
+	{
+
+	}
+}
